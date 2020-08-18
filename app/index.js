b4b1ccb46f7dbf5f589771d2a5c2d22c9705ca17
vikrant singh <vikrant.mongoosetech@gmail.com>
2020-08-18 19:43:48 +0530

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import createReducer from './redux/reducers';
+import App from './containers/App';
+
+const MOUNT_NODE = document.getElementById('app');
+const reducer = createReducer();
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  MOUNT_NODE
+);
